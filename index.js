a35049b09c18fbb7b18d56f8879cd62109820b56
whyiug <whyiug@hotmail.com>
2024-12-10 22:26:39 +0800

    no msg

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const xml2js = require('xml2js')
 
 const app = express()
 
-app.use(express.json())
+app.use(express.raw({ type: 'application/xml' }))
+app.use(express.json({ type: 'application/json' }))
 
 app.post('/', async (req, res) => {
   let logMessage = '消息推送\n'
   if (req.is('application/xml')) {
     const parser = new xml2js.Parser()
-    parser.parseString(req.body, (err, result) => {
+    parser.parseString(req.body.toString(), (err, result) => {
       if (err) {
         logMessage += '解析 XML 失败\n'
       } else {
         logMessage += JSON.stringify(result, null, 2) + '\n'
       }
+      console.log(logMessage)
     })
   } else {
     logMessage += JSON.stringify(req.body, null, 2) + '\n'
+    console.log(logMessage)
   }
-  console.log(logMessage)
   res.send('success') // 不进行任何回复，直接返回success，告知微信服务器已经正常收到。
 })
 
